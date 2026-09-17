11a319b007c4fe4e454763c6a8d3b7e7edf25b59
dadojonako-dot <dadojonako@gmail.com>
2026-09-17 12:10:18 +0500

    Add API bootstrap and health endpoint

---
+++ src/DadoRetail.Api/Program.cs
@@ -0,0 +1,28 @@
+using DadoRetail.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("Postgres")
+    ?? throw new InvalidOperationException("ConnectionStrings:Postgres is required.");
+
+builder.Services.AddDbContext<DadoRetailDbContext>(options => options.UseNpgsql(connectionString));
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks().AddNpgSql(connectionString);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.MapHealthChecks("/health");
+app.Run();
+
+public partial class Program { }
